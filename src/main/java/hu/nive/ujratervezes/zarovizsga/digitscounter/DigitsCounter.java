7633bf349d5afe97d4142b2bd4cadcff7465725b
dudikpal <dudikpal@gmail.com>
2021-03-16 22:06:12 +0100

    zarovizsga DigitsCounter feladat megoldva

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,4 +1,21 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
+    
+    private Set<Integer> digits = new HashSet<>();
+    
+    public int getCountOfDigits(String s) {
+        if (s == null || s.isBlank()) {
+            return 0;
+        }
+        for (char letter : s.toCharArray()) {
+            if (Character.isDigit(letter)) {
+                digits.add(Character.getNumericValue(letter));
+            }
+        }
+        return digits.size();
+    }
 }
